d9903108344561f15a862c8c385910a033a528aa
mohamedhann <devcov2387@gmail.com>
2023-05-02 01:15:15 +0200

    change some pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,13 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 
 
+
+
+Auth::routes(['register' => false]);
+
+//Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/{page}', 'AdminController@index');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
